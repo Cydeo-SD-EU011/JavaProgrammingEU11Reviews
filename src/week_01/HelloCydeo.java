1ca65d969535ec2f27e91e677226eeec45a0944e
AdamB25 <barsialtunit@gmail.com>
2023-02-26 11:02:15 -0500

    week 1

---
+++ src/week_01/HelloCydeo.java
@@ -0,0 +1,12 @@
+package week_01;
+
+public class HelloCydeo {
+
+    // in oder to create main method type "main" hit the ENTER
+    public static void main(String[] args) {
+
+        //in order to show the printl statement you can type "sout" and hit the ENTER
+        System.out.println("Hello Cydeo!");
+    }
+
+}
---
+++ src/week_01/HelloWorld.java
@@ -0,0 +1,14 @@
+package week_01;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        //beautify our codes
+        // ctrl+alt+L for windows
+        //command+option+L for mac
+        System.out.println("Hello World!");
+
+    }
+
+}
