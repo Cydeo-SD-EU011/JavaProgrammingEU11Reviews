37ab745509b5f14a6c01fb0d69535c301041101e
AdamB25 <barsialtunit@gmail.com>
2023-03-05 08:50:24 -0500

    week 3 Task 1 done

--- src/week_03/Task1.java
+++ src/week_03/Task1.java
@@ -12,5 +12,34 @@ public class Task1 {
 
     public static void main(String[] args) {
 
+        //Escape sequences
+        // \n append a new line
+
+        System.out.print("Adam\n");
+        System.out.print("Eyup");
+
+        System.out.println();
+        // \t paragraph space(tab)
+
+        System.out.print("Adam\tEyup\n");
+        // \\ single bf slash
+
+        System.out.println("Computer\\Laptop");
+        // \" I want to add " in my string
+
+      //Car("Mercedes")
+
+        System.out.println("Car(\"Mercedes\")");
+
+        System.out.print("*\n* *\n* * *\n* * * *\n* * * * *");
+
+
+
+
+
+
+
+
+
     }
 }
