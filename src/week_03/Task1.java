36bd07105aa273d856cb1e3ac0e52464550f64b0
AdamB25 <barsialtunit@gmail.com>
2023-03-05 08:31:55 -0500

    week 3 Task 1

---
+++ src/week_03/Task1.java
@@ -0,0 +1,16 @@
+package week_03;
+
+public class Task1 {
+    /*
+    Task 1: Create a class RightTriangle.java and print it (do not use println())
+          *
+          * *
+          * * *
+          * * * *
+          * * * * *
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
